55ff2504fc63536d2555c7be37b02de7320fe55e
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-18 20:50:13 +0200

    Feature: created google maps view

--- moon/src/App.js
+++ moon/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const StyledDiv = styled.div`
+width: 100%;
+height: 100%;
+
+`;
+
+class App extends Component {
+  render() {
+     return (
+      <StyledDiv>
+     <Map google={this.props.google} toom={14}>
+
+<Marker onClick={this.onMarkerClick}
+name={`Current location`} />
+
+<InfoWindow onClose={this.onInfoWindowClose}>
+
+</InfoWindow>
+     </Map>
+    </StyledDiv>
+     )
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: ('AIzaSyBj-zn3g_4sI96N3HleJJI-9QPUPrlx5xc')
+})(App)
+
+// AIzaSyBj-zn3g_4sI96N3HleJJI-9QPUPrlx5xc => KEY
